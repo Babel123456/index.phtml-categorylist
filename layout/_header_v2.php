ddd8d1fa6ad26c3aefcd4eebf20bfc797278badd
Babel123456 <babel@vmage.com.tw>
2019-06-25 10:58:56 +0800

    Update _header_v2.php
    delete mark

--- layout/_header_v2.php
+++ layout/_header_v2.php
@@ -8,8 +8,7 @@
     $url_prefix = 'https://cdn.pinpinbox.com'; //網路上路徑
 	
   ?>
-  <!-- 測試預設值 (B的靜態頁面測試使用) 區塊開始 -->
-
+<!-- 測試預設值 (B的靜態頁面測試使用) 區塊開始 -->
 
 <!doctype html>
 <html lang="zh">
@@ -26,107 +25,96 @@
 <body>
   <script type="text/javascript" src="<?=$url_prefix?>/static_file/pinpinbox/zh_TW/js/jquery-1.11.3.min.js"></script>
   <script type="text/javascript" src="<?=$url_prefix?>/static_file/pinpinbox/zh_TW/js/bootstrap.min.js"></script>
-  <!-- 表頭區塊開始 -->
+ 
+  <!-- 表頭區塊開始 --> 
+  <div id="header_v2" class="header_v2">
   
-  <!-- //20190620 加class V2 -->
-  <div id="header" class="header header_v2">
-  
-  <!-- 手機版搜尋區塊開始 -->
-    <div id="mobile_search">
-	  <div class="header_search">
-		  <div class="header_search_box">
-		     <a href="javascript:void(0);" onclick="_search('mobile')"><i class="fa fa-search"></i></a>
-			  <input type="text" class="search_text" name="searchkey" id="mobile_searchkey" data-searchtype="user" data-device="mobile" autocomplete="off">
-              <div class="search_btn">
-			    <div class="dropdown-sign dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-				  <span class="search_type" data-searchtype="user">創作人</span> <i class="fa fa-caret-down"></i>
-				</div>
-				<div class="dropdown-menu dropdown_menu" role="menu">
-				  <ul>
-					<li><a href="javascript:void(0)"  class="searchtype" data-searchtype="user" data-device="mobile">創作人</a></li>
-			        <li><a href="javascript:void(0)"  class="searchtype" data-searchtype="album" data-device="mobile">作品</a></li>
-				  </ul>
-				</div>
-			  </div>
-		  </div>
+    <div id="header_v2_top" class="header_v2_top">
+	
+	  <div class="top_left_area">
+		<div class="service_link"><a href="#">Service</a></div>
+		<div class="service_link"><a href="#">Content</a></div>
+		<div class="social_link">
+		  <a href="https://www.facebook.com/pinpinboxtw/" title="facebook" target="_blank">
+		    <img src="https://cdn.pinpinbox.com/static_file/pinpinbox/zh_TW/images/assets-v7/social_facebook.png" title="facebook">
+          </a>
 		</div>
-	  <div class="header_search_close"><i class="fa fa-times"></i></div>
-	</div>
-	<!-- 電腦版搜尋區塊開始 -->
-	  <div id="header_top">
+		<div class="social_link">
+		  <a href="https://www.youtube.com/channel/UCAUmAHykldcgI7Vc4OgyOPA" title="youtube" target="_blank">
+		    <img src="images/assets-v7/social_youtube.png">
+		  </a>
+		</div>
+	  </div>
 	  
-		
-		<!-- //20190620 增加"service, contact, fb, youtube"區塊開始 -->
-		<div class="logo_left_area">
-		  <div class="service_link"><a href="#">Service</a></div>
-		  <div class="service_link"><a href="#">Content</a></div>
-		  <div class="social_link">
-		    <a href="https://www.facebook.com/pinpinboxtw/" title="facebook" target="_blank">
-			  <img src="https://cdn.pinpinbox.com/static_file/pinpinbox/zh_TW/images/assets-v7/social_facebook.png" title="facebook">
-            </a>
-		  </div>
-		  <div class="social_link">
-		    <a href="https://www.youtube.com/channel/UCAUmAHykldcgI7Vc4OgyOPA" title="youtube" target="_blank">
-			  <img src="images/assets-v7/social_youtube.png">
-			</a>
-		  </div>
-		</div>
-		<!-- //20190620 增加"service, contact, fb, youtube"區塊開始 -->
-		
-		
-		<div class="header_logo"><a href="https://www.pinpinbox.com/">pinpinbox釘釘吧</a></div>
-		<div class="header_search">
+	  <div class="top_center_area">
+	    <div class="header_logo_v2"><a href="https://www.pinpinbox.com/">pinpinbox釘釘吧</a></div>
+	  </div>
+	  
+	  <div class="top_right_area">
+	    
+		<!-- 搜尋區塊開始 -->
+		<div id="header_search_v2" class="header_search_v2">
 		  <div class="header_search_box">
-			  <!-- //20190620 div排列順序交換區塊開始 -->
 			  <div class="search_btn">
 			    <div class="dropdown-sign dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
 				  <span class="search_type" data-searchtype="user">創作人</span> <i class="fa fa-caret-down"></i>
 				</div>
 				<div class="dropdown-menu dropdown_menu" role="menu">
 				  <ul>
-					<li><a href="javascript:void(0)"  class="searchtype" data-searchtype="user" data-device="desktop">創作人</a></li>
-			        <li><a href="javascript:void(0)"  class="searchtype" data-searchtype="album" data-device="desktop">作品</a></li>
+					<li><a href="javascript:void(0)"  class="searchtype" data-searchtype="user" data-device="">創作人</a></li>
+			        <li><a href="javascript:void(0)"  class="searchtype" data-searchtype="album" data-device="">作品</a></li>
 				  </ul>
 				</div>
 			  </div>
-			  
-			  <input type="text" class="search_text" name="searchkey" id="desktop_searchkey" data-searchtype="user" data-device="desktop" autocomplete="off">
-              
-			  <a href="javascript:void(0);" onclick="_search('desktop')"><i class="fa fa-search"></i></a>
-			  <!-- //20190620 div排列順序交換區塊結束 -->
+			  <div class="search_input_text"><input type="text" class="search_text" name="searchkey" id="searchkey" data-searchtype="user" data-device="" autocomplete="off"></div>
+              <div class="search_icon">
+			    <a href="javascript:void(0);" onclick="_search()"><i class="fa fa-search"></i></a>
+			  </div>
 		  </div>
 		</div>
+		<!-- 搜尋區塊結束 -->
 		
+		
+		<!-- 登入區塊開始 -->
 		<!-- 電腦版選單開始 -->
-		<div class="header_menu">
+		<div class="header_menu_v2">
 		  
 		  <div class="login_menu_area">
 		  
-		    <!-- 未入時-->
+		    <!-- 大版未登入時出現區塊開始-->
+			<!--
 			<div class="sign_link">
 		      <a href="#">Sign in | up</a>
 		    </div>
-		    <!--
-		    <a href="javascript:void(0);" id="login" class="dropdown-sign dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-              <img onerror="this.src='<?=$url_prefix?>/static_file/pinpinbox/zh_TW/images/face_sample.png'" src="<?=$url_prefix?>/storage/zh_TW/user/4581/picture$18d7.jpg">
-            </a>
-		    -->
+			-->
+			<!-- 大版未登入時出現區塊結束-->
+			
+		    
+			<div id="login_link" class="login_link" class="dropdown-sign dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+		      
+			  <a href="javascript:void(0);" id="login" class="login">
+                <img onerror="this.src='<?=$url_prefix?>/static_file/pinpinbox/zh_TW/images/face_sample.png'" src="<?=$url_prefix?>/storage/zh_TW/user/4581/picture$18d7.jpg">
+              </a>
+			  <i id="login_w" class="fa fa-bars login_w"></i>
+			
+			</div>
+		    
 		    <div id="login_menu" class="login_menu dropdown-menu dropdown_menu" role="menu">
 			
 		      <ul>		
-			  　<!-- 有登入會出現區塊開始 -->
-				<li class="login_menu_user_info"><!-- 小版出現 -->
-				      <div class="login_menu_user_info_image">
-					    <div class="profile_img_wrap">
-			              <img class="profile_img" src="https://cdn.pinpinbox.com/storage/zh_TW/user/4069/picture$ea13.jpg">
-		                </div>
-					  </div>
-					  <div class="login_menu_user_info_name">Michael Jordan</div>
-				</li>
-				
+			  　<!-- 已登入會出現區塊開始 -->
 								
 		        <li>
 		            <ul>
+					  <li class="login_menu_user_info">
+				        <div class="login_menu_user_info_image">
+					      <div class="profile_img_wrap">
+			                <img class="profile_img" src="https://cdn.pinpinbox.com/storage/zh_TW/user/4069/picture$ea13.jpg">
+		                  </div>
+					    </div>
+					    <div class="login_menu_user_info_name">Michael Jordan</div>
+				      </li>
+					
 			          <li><a href="creative_content.php#tab0">我的作品</a></li>
 			          <li><a href="creative_content.php#tab1">收藏・贊助</a></li>
 			          <li><a href="creative_content.php#tab2">群組作品</a></li>
@@ -148,47 +136,64 @@
 			          <li><a href="#">回報記錄</a></li>
 			        </ul>
 		        </li>
-				<!-- 有登入會出現區塊結束 -->
 				
+				<!-- 已登入會出現區塊結束 -->
 				
-				<!-- 有無登入都出現區塊開始 -->
-				 <li>
+				<li>
 		            <ul>
+					  <!-- 未登入小版區塊開始 -->
+					  <!--
+				      <li class="sign_link">
+		                <ul>
+			              <li><a href="#">Sign in | up</a></li>
+			            </ul>
+		              </li>
+				      -->
+					  <!-- 未登入小版區塊結束 -->
+					
+					  <!-- 有無登入都出現區塊開始 -->
 			          <li><a href="#">Service</a></li>
 					  <li><a href="#">Contact</a></li>
-			          <li>
-					   <div></div>
-					   <div></div>
+			          <li class="social_link_area">
+					    <div class="social_link">
+		                  <a href="https://www.facebook.com/pinpinboxtw/" title="facebook" target="_blank">
+		                    <img src="https://cdn.pinpinbox.com/static_file/pinpinbox/zh_TW/images/assets-v7/social_facebook.png" title="facebook">
+                          </a>
+		                </div>
+		                <div class="social_link">
+		                  <a href="https://www.youtube.com/channel/UCAUmAHykldcgI7Vc4OgyOPA" title="youtube" target="_blank">
+		                    <img src="images/assets-v7/social_youtube.png">
+		                  </a>
+		                </div>
 					  </li>
+					  <!-- 有無登入都出現區塊結束 -->
 			        </ul>
 		        </li>
 				
-				<!-- 有無登入都出現區塊結束 -->
-				
 		      </ul>
 			  
+			  <!-- 登入後才會出現按鈕: 登入改英文字-->
 			  
-			  <!-- 登入改英文字-->
 		      <a href="#"><div class="btn_new">Sing out</div></a>
 			  
 	        </div>
+		  </div><!-- .login_menu_area -->  
 		  
-		  </div>
-		  
-		</div>
+		</div><!-- .header_menu -->
+		<!-- 電腦版選單結束 -->
+		<!-- 登入區塊結束 -->
 		
-		
-		<!-- 電腦版選單開始 -->
 		<!-- 手機版選單開始 -->
-		<div class="mobile_menu">
-		  <div class="mobile_img"><i id="search_m" class="fa fa-search"></i><i id="notifier_m" class="fa fa-bell"></i>
-		  <!--<a href="https://w3.pinpinbox.com/index/user/login/">-->
-		    <i id="login_m" class="fa fa-bars"></i>
-		  <!--</a>--></div>
+		<div id="mobile_menu_v2" class="mobile_menu_v2">
+		  <div id="search_m" class="search_m"><i class="fa fa-search"></i></div>
+		  <div id="login_m" class="login_m"><i class="fa fa-bars"></i></div>
+		  <div id="header_search_close_v2" class="header_search_close_v2"><i class="fa fa-times"></i></div>
 		</div>
-		<!-- 手機版選單結束 -->
-	  </div>
-	  
-	  
+		<!-- 手機版選單結束 -->		
+		
+	  </div><!-- .top_right_area -->
+	
+	</div>
+	
   </div>
   <!-- 表頭區塊結束 -->
